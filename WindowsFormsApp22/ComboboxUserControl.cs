df0bdbfcdce4de7f4e9dd416306091533ffc5fc6
Kamran Aliyev <kamran88-88@outlook.com>
2018-08-01 19:36:06 +0400

    Add project files.

---
+++ WindowsFormsApp22/ComboboxUserControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp22
+{
+    public partial class ComboboxUserControl : UserControl
+    {
+        public ComboboxUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetCombobox(List<string> list)
+        {
+            comboBox1.Items.AddRange(list.ToArray());
+        }
+    }
+}
---
+++ WindowsFormsApp22/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>() { "A","B","C","D","E"};
+        ComboboxUserControl userControl = new ComboboxUserControl();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            userControl.SetCombobox(list);
+            Controls.Add(userControl);
+        }
+    }
+}
